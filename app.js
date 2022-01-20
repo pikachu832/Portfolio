81cbee659dc028a01575b761ed5523cc779da82b
pikachu832 <76896832+pikachu832@users.noreply.github.com>
2022-01-20 20:24:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-document.getElementById('clarity').onclick = function()
+document.getElementById('clarity').onclick = function before()
 {
     document.body.style.backgroundColor = '#000000';
     document.getElementsByClassName("col-5")[0].style.color ='#ffffff';
+    document.getElementsByClassName("row")[1].style.color ='#ffffff';  
+}
+function afterr(){
+    document.body.style.backgroundColor = '#ffffff';
+    document.getElementsByClassName("col-5")[0].style.color ='#000000';
+    document.getElementsByClassName("row")[1].style.color ='#000000'; 
 }
