43c9e8c92be96edbbc52d6ea66e1cf4321054608
pikachu832 <76896832+pikachu832@users.noreply.github.com>
2022-01-31 05:59:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-document.getElementById('clarity').onclick = function before()
-{
+// let col5 = document.getElementsByClassName("col-5")[0];
+// let row = document.getElementsByClassName("row")[1];  
+
+document.getElementById('clarity').onclick = function (){
+    if(document.body.style.backgroundColor = 'rgb(255, 255, 255);')
+    {
     document.body.style.backgroundColor = '#000000';
     document.getElementsByClassName("col-5")[0].style.color ='#ffffff';
     document.getElementsByClassName("row")[1].style.color ='#ffffff';  
-}
-function afterr(){
+    }
+    else(document.body.style.backgroundColor = '#000000')
+    {
     document.body.style.backgroundColor = '#ffffff';
     document.getElementsByClassName("col-5")[0].style.color ='#000000';
     document.getElementsByClassName("row")[1].style.color ='#000000'; 
+    }
 }
+
+
+
+// document.getElementById('clarity').onclick = function ()
+// {
+//     document.body.style.backgroundColor = '#000000';
+//     document.getElementsByClassName("col-5")[0].style.color ='#ffffff';
+//     document.getElementsByClassName("row")[1].style.color ='#ffffff';  
+// }
+// function (){
+//     document.body.style.backgroundColor = '#ffffff';
+//     document.getElementsByClassName("col-5")[0].style.color ='#000000';
+//     document.getElementsByClassName("row")[1].style.color ='#000000'; 
+// }
