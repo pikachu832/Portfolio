895162a9daa6e2d1f16f6e0e5d643423f158ae44
pikachu832 <76896832+pikachu832@users.noreply.github.com>
2022-01-31 06:43:38 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var links = document.getElementById("clarity");
+var first_click = true;
+links.onclick = function() {
+    if (first_click) {
+        document.body.style.backgroundColor = '#000000';
+        document.getElementsByClassName("col-5")[0].style.color ='#ffffff';
+        document.getElementsByClassName("row")[1].style.color ='#ffffff'; 
+        first_click = false;
+    } else {
+        document.body.style.backgroundColor = '#ffffff';
+        document.getElementsByClassName("col-5")[0].style.color ='#000000';
+        document.getElementsByClassName("row")[1].style.color ='#000000';
+        first_click = true;
+    }
+}
