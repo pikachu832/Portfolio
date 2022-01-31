a623f50ccc53a018325993d7e519e0c5939a0e86
pikachu832 <76896832+pikachu832@users.noreply.github.com>
2022-01-31 06:33:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-// let col5 = document.getElementsByClassName("col-5")[0];
-// let row = document.getElementsByClassName("row")[1];  
-
-document.getElementById('clarity').onclick = function (){
-    if(document.body.style.backgroundColor = 'rgb(255, 255, 255);')
-    {
-    document.body.style.backgroundColor = '#000000';
-    document.getElementsByClassName("col-5")[0].style.color ='#ffffff';
-    document.getElementsByClassName("row")[1].style.color ='#ffffff';  
-    }
-    else(document.body.style.backgroundColor = '#000000')
-    {
-    document.body.style.backgroundColor = '#ffffff';
-    document.getElementsByClassName("col-5")[0].style.color ='#000000';
-    document.getElementsByClassName("row")[1].style.color ='#000000'; 
+var links = document.getElementById("clarity");
+var first_click = true;
+links.onclick = function() {
+    if (first_click) {
+        document.body.style.backgroundColor = '#000000';
+        document.getElementsByClassName("col-5")[0].style.color ='#ffffff';
+        document.getElementsByClassName("row")[1].style.color ='#ffffff'; 
+        first_click = false;
+    } else {
+        document.body.style.backgroundColor = '#ffffff';
+        document.getElementsByClassName("col-5")[0].style.color ='#000000';
+        document.getElementsByClassName("row")[1].style.color ='#000000';
+        first_click = true;
     }
 }
-
-
-
-// document.getElementById('clarity').onclick = function ()
-// {
-//     document.body.style.backgroundColor = '#000000';
-//     document.getElementsByClassName("col-5")[0].style.color ='#ffffff';
-//     document.getElementsByClassName("row")[1].style.color ='#ffffff';  
-// }
-// function (){
-//     document.body.style.backgroundColor = '#ffffff';
-//     document.getElementsByClassName("col-5")[0].style.color ='#000000';
-//     document.getElementsByClassName("row")[1].style.color ='#000000'; 
-// }
