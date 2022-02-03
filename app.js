9821d7b31e2993e3df1d7cc41ff5d6be707a946d
pikachu832 <76896832+pikachu832@users.noreply.github.com>
2022-02-03 18:18:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,9 +19,3 @@ document.getElementById("myBtn").onclick = function() {myFunction()};
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
-
-  document.getElementById("myB421").onclick = function() {myFunction()};
-
-function myFunction() {
-    document.getElementById("myDropdo421").classList.toggle("sh421");
-  }
